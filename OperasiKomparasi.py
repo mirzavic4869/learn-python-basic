c6163a208cba354a9b0e92444b9dc3c69ffd058a
Mirzam Avicena <avicenamirzam@gmail.com>
2024-06-11 23:17:55 +0700

    operasi komarasi

---
+++ OperasiKomparasi.py
@@ -0,0 +1,14 @@
+# >, <, == , is , is not
+
+a = 2
+b = 4
+
+hasil = a > 3
+print(a, '>', 3, '=', hasil)
+
+hasil = a == b
+print(hasil)
+
+# is  tidak dapat digunakan pada literal
+hasil = a is 5
+print(hasil)
