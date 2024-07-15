2c7701e74fd97b49912f51aa7b72f5353c1b3e1d
Mirzam Avicena <avicenamirzam@gmail.com>
2024-07-15 14:42:36 +0700

    import statement

---
+++ ImportStatement.py
@@ -0,0 +1,12 @@
+# import program
+import FungsiTambah
+import OperasiLogika
+
+# import variabel
+import Variabel
+
+print(Variabel.nama)
+
+# import fungsi
+
+print(FungsiTambah.tambah(2, 4))
---
+++ Variabel.py
@@ -0,0 +1,1 @@
+nama = "Mirzam Avicena"
