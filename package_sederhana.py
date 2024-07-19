1c3e045aa6d8f7072ab967637cf0b3777332ba23
Mirzam Avicena <avicenamirzam@gmail.com>
2024-07-19 14:56:46 +0700

    __init__package

--- package_sederhana.py
+++ package_sederhana.py
@@ -1,6 +1,8 @@
 # folder _pycache_ berisi file compiler yang membuat eksekusi kode semakin cepat
 
 
+import package_sederhana
+import package_sederhana.matematika
 import sains.matematika
 from sains import fisika
 import time
@@ -15,3 +17,6 @@ print(t_end-t_start)
 
 gaya = fisika.gaya(2, 4)
 print(f'gaya sebesar = {gaya}')
+
+hasil_kali = package_sederhana.matematika.kali(1, 2, 3)
+print(f'hasil kali adalah = {hasil_kali}')
