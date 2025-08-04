b206a9920ac0a5fb4d167c9912d397704e2b167a
Mirzam Avicena <avicenamirzam@gmail.com>
2025-08-05 03:13:42 +0700

    class variable and instance variable

--- OOP/Main.py
+++ OOP/Main.py
@@ -1,15 +1,20 @@
 class Hero:  # template
+    # class variable
+    jumlah = 0
+
     def __init__(self, inputName, inputHealth, inputPower, inputArmor):
+        # instance variable
         self.name = inputName
         self.health = inputHealth
         self.power = inputPower
         self.armor = inputArmor
+        Hero.jumlah += 1
+        print("Membuat hero dengan nama " + inputName)
 
 
 hero1 = Hero("Sniper", 100, 10, 20)
+print(Hero.jumlah)
 hero2 = Hero("Mirana", 200, 20, 50)
 hero3 = Hero("Sven", 400, 5, 30)
 
-print(hero1.name)
-print(hero2.__dict__)
-print(hero3.armor)
+print(Hero.jumlah)
