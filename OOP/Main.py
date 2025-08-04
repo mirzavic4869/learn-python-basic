43e3728ae0e72946da0a15b2748daba2b8a34123
Mirzam Avicena <avicenamirzam@gmail.com>
2025-08-05 03:30:57 +0700

    Belajar OOP - method

--- OOP/Main.py
+++ OOP/Main.py
@@ -8,13 +8,25 @@ class Hero:  # template
         self.health = inputHealth
         self.power = inputPower
         self.armor = inputArmor
-        Hero.jumlah += 1
-        print("Membuat hero dengan nama " + inputName)
+
+    # void function => method tanpa return, tanpa argumen
+    def siapa(self):
+        print("namaku adalah " + self.name)
+
+    # method dengan argumen, tanpa return
+    def healthUp(self, up):
+        self.health += up
+
+    # method dengan return
+    def getHealth(self):
+        return self.health
 
 
 hero1 = Hero("Sniper", 100, 10, 20)
-print(Hero.jumlah)
 hero2 = Hero("Mirana", 200, 20, 50)
 hero3 = Hero("Sven", 400, 5, 30)
 
-print(Hero.jumlah)
+hero2.siapa()
+hero3.healthUp(20)
+
+print(hero3.getHealth())
